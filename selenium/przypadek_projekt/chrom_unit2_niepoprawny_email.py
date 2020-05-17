a53e2543d59b2233ddabf3fc0849adb58055d8cb
eadg <tester@tester-m>
2020-05-17 10:03:32 +0000

    q

--- selenium/przypadek_projekt/chrom_unit2_niepoprawny_email.py
+++ selenium/przypadek_projekt/chrom_unit2_niepoprawny_email.py
@@ -26,4 +26,48 @@ class WizzairRegistration(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
+    
+    
+'''
+Scenariusz testowy:
+Rejestracja na stronie https://wizzair.com/pl-pl#/
+
+Warunki wstepne:
+Przegladarka otwarta na stronie https://wizzair.com/pl-pl#/
+
+Przypadek testowy:
+Bledny e-mail (Brak znaku '@')
+
+Kroki:
+1. Kliknij "Zalguj sie"
+2. Kliknij "Rejestracja"
+3. Wprowadz imie
+4. Wprowadz nazwisko
+5. Wybierz plec
+6. Wpisz kod kraju
+7. Wpisz numer telefonu
+8. Wpisz bledny e-mail
+9. Wpisz haslo
+10. Wybierz kraj
+11. Zaakceptuj polityke prywatnisci
+12. Kliknij Zarejestruj sie
+
+Oczekiwany rezultat:
+1. Uzytkownik dostaje informacje "Nieprawidłowy adres e-mail"
+
+
+
+FULL XPATH:
+/html/body/div[2]/div/header/div[1]/div/nav/ul/li[11]/button
+
+COPIED XPATH:
+//*[@id="app"]/div/header/div[1]/div/nav/ul/li[11]/button
+
+MY XPATH:
+//button[@data-test="navigation-menu-signin"]
+
+CSS SELECTOR:
+button[data-test="navigation-menu-signin"]
+'''
+
 
